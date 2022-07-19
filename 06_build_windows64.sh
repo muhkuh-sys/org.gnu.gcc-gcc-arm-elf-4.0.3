1f5737edc40e312520e9258dd53d1b004b35af0d
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2022-07-19 16:20:06 +0200

    Add Windows x86 and x86_64 builds.

---
+++ 06_build_windows64.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -e
+
+. ./configuration.sh
+
+mkdir -p ${BUILD_DIR_BUILD}/windows_x86_64/gcc-${GCC_VER}
+mkdir -p ${BUILD_DIR_BUILD}/windows_x86_64/binutils-${BINUTILS_VER}
+
+
+################
+# Build Binutils
+################
+
+echo "=================================="
+echo "Building binutils-${BINUTILS_VER}"
+echo "=================================="
+
+pushd ${BUILD_DIR_BUILD}/windows_x86_64/binutils-${BINUTILS_VER}
+${BUILD_DIR_SRC}/binutils-${BINUTILS_VER}/configure --target=${ARM_TARGET} --prefix=${WINDOWS_X86_64_PREFIX} --enable-interwork --enable-multilib --disable-shared --disable-nls --enable-werror=no --host=x86_64-w64-mingw32 --build=x86_64-unknown-linux-gnu
+make all install
+popd
+
+export PATH=${PATH}:${LINUX_PREFIX}/bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LINUX_PREFIX}/lib
+
+
+################
+# Build gcc-main
+################
+
+echo "=================================="
+echo "Building GCC-${GCC_VER} (main)"
+echo "=================================="
+
+# Copy the specs file from the linux version.
+mkdir -p ${BUILD_DIR_BUILD}/windows_x86_64/gcc-${GCC_VER}/gcc
+cp ${BUILD_DIR_BUILD}/linux/gcc-${GCC_VER}/gcc/xgcc ${BUILD_DIR_BUILD}/windows_x86_64/gcc-${GCC_VER}/gcc/
+
+# Copy the fixincl tool.
+mkdir -p ${BUILD_DIR_BUILD}/windows_x86_64/gcc-4.0.4/build-x86_64-w64-mingw32/fixincludes
+cp ${BUILD_DIR_BUILD}/linux/gcc-4.0.4/fixincludes/fixincl ${BUILD_DIR_BUILD}/windows_x86_64/gcc-4.0.4/build-x86_64-w64-mingw32/fixincludes/
+
+mkdir -p ${WINDOWS_X86_64_PREFIX}/${ARM_TARGET}
+mkdir -p ${WINDOWS_X86_64_PREFIX}/${ARM_TARGET}
+mkdir -p ${WINDOWS_X86_64_PREFIX}/share
+mkdir -p ${WINDOWS_X86_64_PREFIX}/lib
+cp -r ${LINUX_PREFIX}/${ARM_TARGET}/lib     ${WINDOWS_X86_64_PREFIX}/${ARM_TARGET}/
+cp -r ${LINUX_PREFIX}/${ARM_TARGET}/include ${WINDOWS_X86_64_PREFIX}/${ARM_TARGET}/
+cp -r ${LINUX_PREFIX}/share                 ${WINDOWS_X86_64_PREFIX}/
+cp -r ${LINUX_PREFIX}/lib/gcc               ${WINDOWS_X86_64_PREFIX}/lib
+
+pushd ${BUILD_DIR_BUILD}/windows_x86_64/gcc-${GCC_VER}
+CC=x86_64-w64-mingw32-gcc ${BUILD_DIR_SRC}/gcc-${GCC_VER}/configure --target=${ARM_TARGET} --prefix=${WINDOWS_X86_64_PREFIX} --enable-interwork --enable-multilib --enable-languages="c,c++" --with-newlib --without-headers --disable-libstdcxx-pch --disable-shared --disable-nls --disable-libssp --disable-libmudflap --disable-libgomp --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --host=x86_64-w64-mingw32 --build=x86_64-unknown-linux-gnu
+make all-gcc install-gcc
+popd
---
+++ docker_build_windows.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+set -e
+
+. configuration.sh
+
+# This is the name of the container.
+IMAGE=ghcr.io/muhkuh-sys/mbs_ubuntu_2204_x86_64:latest
+
+# Get the project directory.
+HOSTDIR=`pwd`
+WORKDIR=/tmp/work
+
+# Make sure that the "build" folder exists.
+# NOTE: do not remove it, maybe there are already components.
+mkdir -p ${HOSTDIR}/targets
+
+# Start the container and mount the project folder.
+CONTAINER=`docker run -t -d -v ${HOSTDIR}:${WORKDIR} ${IMAGE} /bin/bash`
+
+# Prepare the build folder.
+docker exec ${CONTAINER} /bin/bash -c "rm -rf ${BUILD_DIR}"
+docker exec ${CONTAINER} /bin/bash -c "mkdir ${BUILD_DIR}"
+
+# This is needed to install wine.
+docker exec ${CONTAINER} /bin/bash -c 'dpkg --add-architecture i386'
+
+# Update the package list to prevent "not found" messages.
+docker exec ${CONTAINER} /bin/bash -c 'apt-get update --assume-yes'
+
+# Accept the license of the msttcorefonts.
+docker exec ${CONTAINER} /bin/bash -c 'echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections'
+
+# Install the project specific packages.
+docker exec ${CONTAINER} /bin/bash -c "apt-get install --assume-yes bzip2 texinfo wine zip"
+
+# Build the linux version.
+# This is necessary to get the tools to build the libraries.
+docker exec ${CONTAINER} /bin/bash -c "cd ${WORKDIR} && bash ./02_clean.sh"
+docker exec ${CONTAINER} /bin/bash -c "cd ${WORKDIR} && bash ./03_extract_patch.sh"
+docker exec ${CONTAINER} /bin/bash -c "cd ${WORKDIR} && bash ./04_build_linux.sh"
+
+# Build the Windows 32bit version.
+docker exec ${CONTAINER} /bin/bash -c "cd ${WORKDIR} && bash ./05_build_windows32.sh"
+
+# Build the Windows 64bit version.
+docker exec ${CONTAINER} /bin/bash -c "cd ${WORKDIR} && bash ./06_build_windows64.sh"
+
+# Create an archive.
+docker exec ${CONTAINER} /bin/bash -c ". ${WORKDIR}/configuration.sh && cd ${WORKDIR}/build/install/windows_x86 && zip -9 --recurse-paths ${WORKDIR}/build/${ARTIFACT_NAME}-windows-x86.zip ."
+docker exec ${CONTAINER} /bin/bash -c ". ${WORKDIR}/configuration.sh && cd ${WORKDIR}/build/install/windows_x86_64 && zip -9 --recurse-paths ${WORKDIR}/build/${ARTIFACT_NAME}-windows-x86_64.zip ."
+
+# Copy the archive from the container to the host.
+docker cp ${CONTAINER}:${BUILD_DIR}/${ARTIFACT_NAME}-windows-x86.tar.gz targets/
+docker cp ${CONTAINER}:${BUILD_DIR}/${ARTIFACT_NAME}-windows-x86_64.tar.gz targets/
+
+# Stop and remove the container.
+docker stop ${CONTAINER}
+docker rm ${CONTAINER}
